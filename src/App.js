819a3aa16f2786ef9183e5cc0ca3281a40b47887
mahmood wirya <mahmudhaji28@yahoo.com>
2022-08-03 16:06:07 +0300

    config: add route definition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+// import Navbar from "./components/Navbar";
+import { Routes, Route} from "react-router-dom"
 
+
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Carts from "./Pages/Carts"
+import Login from "./Pages/Login"
+import Register from "./Pages/Register"
+import Product from "./Pages/Product"
+import Products from "./Pages/Products"
 function App() {
   return (
     <div className="App">
-  <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="Carts" element={<Carts />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="Register" element={<Register />} />
+        <Route path="Products" element={<Products />} />
+        <Route path="Products/:id" element={<Product />} />
+        </Routes>
     </div>
   );
 }
