b2a29bce8722a49764eb26d3da526c1496be07a1
mahmood wirya <mahmudhaji28@yahoo.com>
2022-08-18 14:33:56 +0300

    add:loading spinner feature as a component

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,9 +1,37 @@
-import React from 'react'
-import Layout from '../Layout/MainLayouts'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import LoadingIcon from "../components/LoadingIcon";
+import Layout from "../Layout/MainLayouts";
 export default function Products() {
+
+const [products,setProducts]= useState();
+const [isLoading,setIsLoading]=useState(true);
+
+
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products`)
+      .then((response) => {
+        
+        console.log("list of products:",response.data)
+        setProducts(response.data)
+        setIsLoading(false);
+
+      })
+      .catch((arr) => {
+        console.log(arr);
+        
+      });
+  }, []);
+  if (isLoading){
+    return(
+    <LoadingIcon />
+    )
+  }
+
   return (
     <Layout>
-    <div>Products</div>
+      products
     </Layout>
-  )
+  );
 }
