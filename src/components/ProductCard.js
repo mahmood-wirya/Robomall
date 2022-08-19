5906fefd3877258aebe32b64cbf761190f491e17
mahmood wirya <mahmudhaji28@yahoo.com>
2022-08-19 23:20:10 +0300

    add some design for the product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { delimiter } from "../utils/delimiter";
 
 export default function ProductCard({ product }) {
   return (
-    <div className="col-span-3 bg-white p-7 rounded-t-2xl w-[350px] h-full mt-10 shadow-lg">
-      <p className="tracking-widest text-xs title-font font-medium text-gray-300 mb-3">
+    <Link to={`/products/${product.id}`} className="col-span-3 bg-white p-7 rounded-3xl w-[350px] h-full mt-10 shadow-lg">
+      <p className="tracking-widest text-xs title-font font-medium text-gray-400 mb-3">
         In stock
       </p>
       <img
@@ -11,12 +13,16 @@ export default function ProductCard({ product }) {
         className="h-[200px] w-full object-contain shadow-md rounded-3xl"
       />
       <h2 className="text-lg text-gray-900 font-semibold mt-6">
-        {product.title}
+      {delimiter(product.title,29)}
       </h2>
 
-      <p className="text-base text-gray-500 font-medium mt-4">
-        {product.description}
+      <p className="text-base text-gray-500 font-medium mt-4 mb-3">
+        {delimiter(product.description,90)}
       </p>
-    </div>
+      <div className="flex justify-between">
+      <p className="tracking-widest text-xl title-font font-medium text-gray-700">{product.price}$</p>
+      <div className="border-2 rounded-3xl border-[#2DBCB7] text-[#2DBCB7] p-1">{product.category}</div>
+      </div>
+    </Link>
   );
 }
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 
 export default function ProductsList({ products }) {
   return (
-    <div className="container mx-auto grid grid-cols-12 gap-[40px]">
+    <div className=" flex-wrap container mx-auto grid grid-cols-12 gap-[40px]">
       {products.map((product) => {
         return <ProductCard product={product} />;
       })}
