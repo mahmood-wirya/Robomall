a12ea7e53b9a5c88310daa9ceb80abd4ad3e21bf
mahmood wirya <mahmudhaji28@yahoo.com>
2022-09-05 23:24:22 +0300

    hotfix for key error

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -2,11 +2,12 @@ import React from "react";
 import ProductCard from "./ProductCard";
 
 export default function ProductsList({ products }) {
+ 
   return (
     <div>
       <div className="flex flex-wrap container mx-auto gap-x-8 justify-center mb-14 ">
         {products.map((product) => {
-          return <ProductCard  productCard={product} />;
+          return <ProductCard key={product.id} productCard={product} />;
         })}
       </div>
     </div>
